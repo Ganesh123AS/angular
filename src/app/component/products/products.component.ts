9e1621cb7a26b36f9d7341488da0d90aeca5a72a
ganesh bhatt <@ganesh.bhatta>
2024-03-13 19:48:42 +0545

    msg

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, Input, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
 import { CartService } from 'src/app/service/cart.service';
@@ -9,9 +8,9 @@ import { CartService } from 'src/app/service/cart.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  public productsList: any;
+  public productsList: any[];
   @Input() indicators = true;
-  selectedIndex = 1;
+  selectedIndex = 0;
 
   constructor(private api: ApiService, private cartService: CartService) { }
 
@@ -29,17 +28,30 @@ export class ProductsComponent implements OnInit {
     );
   }
 
+  get infiniteIndex(): number {
+    if (this.productsList && this.productsList.length > 0) {
+      const len = this.productsList.length;
+      return (len + this.selectedIndex % len) % len;
+    }
+    return 0;
+  }
+
+  get selectedProduct(): any {
+    if (this.productsList && this.productsList.length > 0) {
+      return this.productsList[this.infiniteIndex];
+    }
+    return null;
+  }
+
   addToCart(item: any) {
     this.cartService.addToCart(item);
   }
+
   nextItem() {
-    if (this.selectedIndex < this.productsList.length - 1) {
-      this.selectedIndex++;
-    }
+    this.selectedIndex = (this.selectedIndex + 1) % this.productsList.length;
   }
+
   prevItem() {
-    if (this.selectedIndex > 0) {
-      this.selectedIndex--;
-    }
+    this.selectedIndex = (this.selectedIndex - 1 + this.productsList.length) % this.productsList.length;
   }
 }
