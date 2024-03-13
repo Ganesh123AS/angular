783123fb65faeb971b787360d2c0a484aaa43996
ganesh bhatt <@ganesh.bhatta>
2024-03-13 18:39:19 +0545

    folder structured made...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ButtonComponent } from './component/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
